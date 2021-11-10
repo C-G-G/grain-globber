eb5ddee86e2bfd60fc21ad8548d05923a1d75c2d
CGG <16348315+C-G-G@users.noreply.github.com>
2021-11-09 21:41:26 -0500

    Create layout with jQuery and CSS

---
+++ js/main.js
@@ -0,0 +1,18 @@
+// Loop over the grains variable and use jQuery to output li tags into the ul
+// Each li tag should have img, h2 and p
+
+var $ul = $('ul');
+
+grains.forEach(function (grain) {
+  var $li = $('<li>');
+  var $img = $('<img>');
+  var $h2 = $('<h2>');
+  var $p = $('<p>');
+
+  $h2.html(grain.name);
+  $p.html(grain.desc);
+  $img.attr('src', 'images/' + grain.img);
+
+  $li.append($img, $h2, $p);
+  $ul.append($li);
+});
